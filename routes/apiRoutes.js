d3d74a4e5538bad981dc2389bb3f9c60a3fec9c3
MJB <matt.berti13@gmail.com>
2022-04-22 11:26:41 -0500

    added server file

---
+++ routes/apiRoutes.js
@@ -0,0 +1,4 @@
+const fs = require('fs');
+const express = ('express');
+
+const route = express.Router();
---
+++ server.js
@@ -0,0 +1,32 @@
+
+const path = require('path');
+const express = require('express');
+const app = express();
+const apiRoutes = require('./routes/apiRoutes.js');
+
+const PORT = process.env.PORT || 3001;
+
+app.use(express.static('public'));
+
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.json());
+
+
+app.use('/api', apiRoutes);
+
+//route for notes.html
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/notes.html'));
+  });
+
+//wildcard route to catch invalid requests(nonexistent URL locations) and send back to index
+//Wildcards should always come last in requests
+app.get('*', (req, res) =>{
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+//Creates API Server at Port 3001
+app.listen(PORT, () =>{
+    console.log(`API server now on port ${PORT}!`);
+});
